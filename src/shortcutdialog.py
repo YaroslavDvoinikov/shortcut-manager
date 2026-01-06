110dd826ba0b2a1f803f4623d260b8ee7338b67d
artyom-fedosov <artjomsfedosovs2@gmail.com>
2026-01-06 19:01:42 +0200

    Add missing files

---
+++ src/shortcutdialog.py
@@ -0,0 +1,47 @@
+from PySide6 import QtWidgets
+
+from src.shortcuts import Shortcuts
+
+
+class ShortcutDialog(QtWidgets.QDialog):
+    def __init__(self, parent, combination):
+        super().__init__(parent)
+        self.__shortcuts = Shortcuts()
+        self.__shortcut = self.__shortcuts.get_shortcuts()[combination]
+
+        self.setWindowTitle("Edit Shortcut")
+
+        self.name_title = QtWidgets.QLabel("Name of a shortcut:")
+        self.name_input = QtWidgets.QLineEdit(text=self.__shortcut.name)
+        self.name_input.setPlaceholderText("Enter name")
+
+        self.description_title = QtWidgets.QLabel("Description of a shortcut:")
+        self.description_input = QtWidgets.QLineEdit(text=self.__shortcut.description)
+        self.description_input.setPlaceholderText("Enter description")
+
+        self.delete_button = QtWidgets.QPushButton("Delete")
+        self.delete_button.clicked.connect(self.delete_shortcut)
+
+        self.save_button = QtWidgets.QPushButton("Save")
+        self.save_button.clicked.connect(self.close_dialog)
+
+        main_layout = QtWidgets.QVBoxLayout(self)
+
+        main_layout.addWidget(self.name_title)
+        main_layout.addWidget(self.name_input)
+        main_layout.addWidget(self.description_title)
+        main_layout.addWidget(self.description_input)
+        main_layout.addWidget(self.delete_button)
+        main_layout.addWidget(self.save_button)
+
+    def delete_shortcut(self):
+        self.__shortcuts.remove(self.__shortcut.combination)
+        self.__shortcuts.save_shortcuts_file("shortcuts.csv")
+        self.accept()
+
+    def close_dialog(self):
+        self.__shortcut.name = self.name_input.text()
+        self.__shortcut.description = self.description_input.text()
+        self.__shortcuts.update(self.__shortcut.combination, self.__shortcut)
+        self.__shortcuts.save_shortcuts_file("shortcuts.csv")
+        self.accept()
