29b32342ec1938ef947545aa6eeee4e8b15a21cc
artyom-fedosov <artjomsfedosovs2@gmail.com>
2026-01-09 21:20:59 +0200

    Recording audio

--- src/actions.py
+++ src/actions.py
@@ -1,15 +1,23 @@
 import os
 import subprocess
 import sys
-from calendar import c
 from datetime import datetime
 from pathlib import Path
 
 import mss
 import pyscreenrec
+import pyaudio
+import wave
 
 recorder = pyscreenrec.ScreenRecorder()
 screen_record_started = False
+
+pa = pyaudio.PyAudio()
+stream = pa.open(format=pyaudio.paInt16, channels=2,
+                 rate=44400, input=True,
+                 frames_per_buffer=1024)
+frames = []
+audio_record_started = False
 
 
 def action(command: int, *args):
@@ -64,17 +72,40 @@ def action(command: int, *args):
                 os.system("pkill -KILL -u" + os.getlogin())
             elif sys.platform == "win32":
                 subprocess.run(["shutdown", "/l", "/t", "0"])
+        case 6:
+            global audio_record_started
+            global frames
+            if not audio_record_started:
+                audio_record_started = True
+                frames = []
+                for i in range(0, int(44400 / 1024 * 4)):
+                    data = stream.read(1024)
+                    frames.append(data)
+            else:
+                audio_record_started = False
+                stream.stop_stream()
+                stream.close()
+                pa.terminate()
+
+                dir_path = Path(args[0])
+                now = datetime.now()
+                filename = (
+                        dir_path
+                        / f"Screenshot-{now.year}-{now.month:02d}-{now.day:02d}-{now.hour:02d}{now.minute:02d}{now.second:02d}.wav"
+                )
+
+                sf = wave.open(str(filename), 'wb')
+                sf.setnchannels(2)
+                sf.setsampwidth(pa.get_sample_size(pyaudio.paInt16))
+                sf.setframerate(44400)
+                sf.writeframes(b''.join(frames))
+                sf.close()
 
 
 def info(command: int):
-    if command == 0:
-        return "Run an executable"
-    elif command == 1:
-        return "Take screenshot"
-    elif command == 2:
-        return "Start recording screen"
-    elif command == 3:
-        return "Stop recording screen"
+    text = ["Run an executable", "Take screenshot", "Start recording screen", "Power off", "Reboot", "Log out",
+            "Record audio"]
+    return text[command]
 
 
 def stop_recording():
